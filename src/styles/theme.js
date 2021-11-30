5681fb4b607c0fd9d2afd0cf28beb61b7a2dfa83
Leo César <leo_cesa@hotmail.com>
2021-11-30 01:38:29 -0300

    feat: Theme created

---
+++ src/styles/theme.js
@@ -0,0 +1,18 @@
+import { createTheme } from "@material-ui/core";
+
+
+export const theme = createTheme({
+    palette:{
+        primary:{
+            main: "#543213",
+            light: "#854354",
+            dark: "#233250"
+        },
+        secondary:{
+            main: "#d9d9d9",
+            light: "#fafafa",
+            dark: "#333444"
+        }
+        
+    }
+})
